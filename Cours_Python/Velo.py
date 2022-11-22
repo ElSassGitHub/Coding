39f09a22bcb2ca48749473dbbad87d051ab2c922
ElSass <elsass69420@gmail.com>
2022-11-22 15:35:05 +0100

    commit

--- Cours_Python/Velo.py
+++ Cours_Python/Velo.py
@@ -6,20 +6,21 @@ heures_high = 0
 tarif_low = 0
 tarif_high = 0
 
-while start < 0 or start > 24 or end < 0 or end > 24:
-    print("Les heures doivent être comprises entre 0 et 24 !\n")
-    start = int(input("Donnez l'heure de début de la location (un entier) : "))
-    end = int(input("Donnez l'heure de fin de la location (un entier) : "))
-
-while start == end:
-    print("Attention ! L'heure de fin est identique à l'heure de début.\n")
-    start = int(input("Donnez l'heure de début de la location (un entier) : "))
-    end = int(input("Donnez l'heure de fin de la location (un entier) : "))
-
-while start > end:
-    print("Attention ! Le début de la location est après la fin ...\n")
-    start = int(input("Donnez l'heure de début de la location (un entier) : "))
-    end = int(input("Donnez l'heure de fin de la location (un entier) : "))
+while True:
+    if start < 0 or start > 24 or end < 0 or end > 24:
+        print("Les heures doivent être comprises entre 0 et 24 !\n")
+        start = int(input("Donnez l'heure de début de la location (un entier) : "))
+        end = int(input("Donnez l'heure de fin de la location (un entier) : "))
+    elif start == end:
+        print("Attention ! L'heure de fin est identique à l'heure de début.\n")
+        start = int(input("Donnez l'heure de début de la location (un entier) : "))
+        end = int(input("Donnez l'heure de fin de la location (un entier) : "))
+    elif start > end:
+        print("Attention ! Le début de la location est après la fin ...\n")
+        start = int(input("Donnez l'heure de début de la location (un entier) : "))
+        end = int(input("Donnez l'heure de fin de la location (un entier) : "))
+    else:
+        break
 
 for i in range(start, end+1):
     if i > 7 and i <= 17:
